241ad7ed038a851ad615ec6dd29d8f5d06033fef
oliv1008 <omillochau@gmail.com>
2019-02-03 23:22:02 +0100

    final

--- model_student/hashtag.php
+++ model_student/hashtag.php
@@ -1,8 +1,10 @@
 <?php
 namespace Model\Hashtag;
+
 use \Db;
 use \PDOException;
 use \PDO;
+
 /**
  * Hashtag model
  *
@@ -14,37 +16,39 @@ use \PDO;
  * @param pid the post id to which attach the hashtag
  * @param hashtag_name the name of the hashtag to attach
  */
-function attach($pid, $hashtag_name) {
-	$db = Db::dbc();
+function attach($pid, $hashtag_name)
+{
+    $db = \Db::dbc();
 
-	$query = $db->prepare("INSERT INTO hashtag(nameHashtag) VALUES(:hashtag_name)");
-	$query->bindValue(":hashtag_name", $hashtag_name);
-	$query->execute();
-		
-	$idHashtag = $db->lastInsertId();
-		
-	$query = $db->prepare("INSERT INTO useHashtag(idHashtag, idTweet) VALUES(:idHashtag, :pid)");
-	$query->bindValue(":idHashtag", $idHashtag);
-	$query->bindValue(":pid", $pid);
-	$query->execute();
-	return true;
+    $query = $db->prepare("INSERT INTO hashtag(nameHashtag) VALUES(:hashtag_name)");
+    $query->bindValue(":hashtag_name", $hashtag_name);
+    $query->execute();
+
+    $idHashtag = $db->lastInsertId();
+
+    $query = $db->prepare("INSERT INTO useHashtag(idHashtag, idTweet) VALUES(:idHashtag, :pid)");
+    $query->bindValue(":idHashtag", $idHashtag);
+    $query->bindValue(":pid", $pid);
+    $query->execute();
+
+    return true;
 }
 
 /**
  * List hashtags
  * @return a list of hashtags names
  */
-function list_hashtags() {
-	$db = \Db::dbc();
+function list_hashtags()
+{
+    $db = \Db::dbc();
 
     $hashtags = [];
 
     $query = $db->prepare("SELECT DISTINCT nameHashtag FROM hashtag");
     $query->execute();
 
-    while ($result = $query->fetch())
-    {
-      $hashtags[] = $result["nameHashtag"];
+    while ($result = $query->fetch()) {
+        $hashtags[] = $result["nameHashtag"];
     }
 
     return $hashtags;
@@ -55,20 +59,20 @@ function list_hashtags() {
  * @param length number of hashtags to get at most
  * @return a list of hashtags
  */
-function list_popular_hashtags($length) {
-	$db = \Db::dbc();
+function list_popular_hashtags($length)
+{
+    $db = \Db::dbc();
 
     $hashtags = [];
-  
+
     $query = $db->prepare("SELECT nameHashtag FROM hashtag GROUP BY nameHashtag ORDER BY count(nameHashtag) DESC");
     $query->execute();
-  
-    while (($result = $query->fetch()) && $length > 0) 
-    {
-    	$length = $length - 1;
-   		$hashtags[] = $result["nameHashtag"];
+
+    while (($result = $query->fetch()) && $length > 0) {
+        $length = $length - 1;
+        $hashtags[] = $result["nameHashtag"];
     }
-    var_dump($hashtags);            
+
     return $hashtags;
 }
 
@@ -77,25 +81,24 @@ function list_popular_hashtags($length) {
  * @param hashtag the hashtag name
  * @return a list of posts objects or null if the hashtag doesn't exist
  */
-function get_posts($hashtag_name) {
-	$db = \Db::dbc();
+function get_posts($hashtag_name)
+{
+    $db = \Db::dbc();
 
     $posts = [];
 
-    $query = $db->prepare("SELECT DISTINCT t.* FROM hashtag NATURAL JOIN tweet t WHERE nameHashtag LIKE :hashtag_name");
-    $query->bindValue(":hashtag_name", "%".$hashtag_name."%");
+    $query = $db->prepare("SELECT DISTINCT * FROM tweet NATURAL JOIN useHashtag NATURAL JOIN hashtag WHERE nameHashtag = :hashtag_name");
+    $query->bindValue(":hashtag_name", $hashtag_name);
     $query->execute();
 
-    while ($result = $query->fetch())
-    {
-        $post = (object) array
-    	(
-        	"id" => $result["idTweet"],
+    while ($result = $query->fetch()) {
+        $post = (object) array(
+                   "id" => $result["idTweet"],
             "text" => $result["content"],
             "date" => $result["dateTweet"],
             "author" => \Model\User\get($result["idUser"])
-    	);
-    	$posts[] = $post;
+        );
+        $posts[] = $post;
     }
 
     return $posts;
@@ -106,6 +109,20 @@ function get_posts($hashtag_name) {
  * @param length the size of the returned list at most
  * @return an array of hashtags names
  */
-function get_related_hashtags($hashtag_name, $length) {
-    return ["Hello"];
+function get_related_hashtags($hashtag_name, $length)
+{
+    $db = \Db::dbc();
+
+    $hashtags = [];
+
+    $query = $db->prepare("SELECT DISTINCT * FROM hashtag NATURAL JOIN useHashtag u WHERE nameHashtag != :hashtag_name AND u.idTweet IN (SELECT idTweet FROM useHashtag NATURAL JOIN hashtag WHERE nameHashtag = :hashtag_name)");
+    $query->bindValue(":hashtag_name", $hashtag_name);
+    $query->execute();
+
+    while (($result = $query->fetch()) && $length > 0) {
+        $length = $length - 1;
+        $hashtags[] = $result["nameHashtag"];
+    }
+
+    return $hashtags;
 }
--- model_student/post.php
+++ model_student/post.php
@@ -1,8 +1,10 @@
 <?php
 namespace Model\Post;
+
 use \Db;
 use \PDOException;
 use \PDO;
+
 /**
  * Post
  *
@@ -16,22 +18,27 @@ use \PDO;
  * @warning the author attribute is a user object
  * @warning the date attribute is a DateTime object
  */
-function get($id) {
+function get($id)
+{
     $db = \Db::dbc();
 
     $query = $db->prepare("SELECT * FROM tweet WHERE idTweet = :id");
     $query->bindValue(":id", $id);
     $query->execute();
+
     $result = $query->fetch();
-    if ($result == NULL) return NULL;
-  	$post = (object) array
-	(
-		"id" => $result["idTweet"],
-		"text" => $result["content"],
-		"date" => $result["dateTweet"],
-		"author" => \Model\User\get($result["idUser"])
-	);
-  
+
+    if ($result == null) {
+        return null;
+    }
+
+    $post = (object) array(
+        "id" => $result["idTweet"],
+        "text" => $result["content"],
+        "date" => $result["dateTweet"],
+        "author" => \Model\User\get($result["idUser"])
+    );
+
     return $post;
 }
 
@@ -45,29 +52,32 @@ function get($id) {
  * @warning the hashtags attribute is an of hashtags objects
  * @warning the responds_to attribute is either null (if the post is not a response) or a post object
  */
-function get_with_joins($id) {
+function get_with_joins($id)
+{
     $db = \Db::dbc();
 
-	
     $query = $db->prepare("SELECT * FROM tweet WHERE idTweet = :id");
     $query->bindValue(":id", $id);
     $query->execute();
+
     $result = $query->fetch();
-    if ($result == NULL) return NULL;
-    $post = (object) array
-	(
-		"id" => $result["idTweet"],
-		"text" => $result["content"],
-		"date" => $result["dateTweet"],
-		"author" => \Model\User\get($result["idUser"])
-	);
-	$post->responds_to = get($result["idTweet_to"]);
-	$post->likes = get_likes($post->id);
-	$post->hashtags = [];
-  
+
+    if ($result == null) {
+        return null;
+    }
+    $post = (object) array(
+        "id" => $result["idTweet"],
+        "text" => $result["content"],
+        "date" => $result["dateTweet"],
+        "author" => \Model\User\get($result["idUser"])
+    );
+    $post->responds_to = get($result["idTweet_to"]);
+    $post->likes = get_likes($post->id);
+    $post->hashtags = [];
+
     return $post;
 }
- 
+
 /**
  * Create a post in db
  * @param author_id the author user's id
@@ -80,29 +90,32 @@ function get_with_joins($id) {
  * @warning this function adds the hashtags
  * @warning this function takes care to rollback if one of the queries comes to fail.
  */
-function create($author_id, $text, $response_to=null) {
-	$db = \Db::dbc();
-	$query = $db->prepare("INSERT INTO tweet(idUser, idTweet_to, content, dateTweet) VALUES(:author_id, :response_to, :text, now())");
-	$query->bindValue(":author_id", $author_id);
-	$query->bindValue(":response_to", $response_to);
-	$query->bindValue(":text", $text);
-	$query->execute();
-	$idPost = $db->lastInsertId();
-	
-	$userMentionned = extract_mentions($text);
-	foreach ($userMentionned as $username) 
-	{
-		$user = \Model\User\get_by_username(ltrim($username, '@'));
-		if($user && $user->id)
-			mention_user($idPost, $user->id);
-	}
-	$hashtags = extract_hashtags($text);
-	foreach ($hashtags as $hashtag) 
-	{
-		\Model\Hashtag\attach($idPost, $hashtag);
-	}
-	
-	return $idPost;
+function create($author_id, $text, $response_to=null)
+{
+    $db = \Db::dbc();
+
+    $query = $db->prepare("INSERT INTO tweet(idUser, idTweet_to, content, dateTweet) VALUES(:author_id, :response_to, :text, now())");
+    $query->bindValue(":author_id", $author_id);
+    $query->bindValue(":response_to", $response_to);
+    $query->bindValue(":text", $text);
+    $query->execute();
+
+    $idPost = $db->lastInsertId();
+
+    $userMentionned = extract_mentions($text);
+    foreach ($userMentionned as $username) {
+        $user = \Model\User\get_by_username(ltrim($username, '@'));
+        if ($user && $user->id) {
+            mention_user($idPost, $user->id);
+        }
+    }
+
+    $hashtags = extract_hashtags($text);
+    foreach ($hashtags as $hashtag) {
+        \Model\Hashtag\attach($idPost, $hashtag);
+    }
+
+    return $idPost;
 }
 
 /**
@@ -110,13 +123,15 @@ function create($author_id, $text, $response_to=null) {
  * @param pid the post id
  * @param uid the user id to mention
  */
-function mention_user($pid, $uid) {
-	$db = Db::dbc();
+function mention_user($pid, $uid)
+{
+    $db = \Db::dbc();
 
     $query = $db->prepare("INSERT INTO mention (idUser, idTweet) VALUES(:uid, :pid)");
     $query->bindValue(":uid", $uid);
     $query->bindValue(":pid", $pid);
     $query->execute();
+
     return true;
 }
 
@@ -125,40 +140,43 @@ function mention_user($pid, $uid) {
  * @param pid the post id
  * @return the array of user objects mentioned
  */
-function get_mentioned($pid) {
+function get_mentioned($pid)
+{
     $db = \Db::dbc();
-	
-	$users = [];
-	$query = $db->prepare("SELECT * FROM mention m INNER JOIN user u ON u.idUser = m.idUser WHERE m.idTweet = :pid");
-	$query->bindValue(":pid", $pid);
-	$query->execute();
-	while ($result = $query->fetch()) 
-	{
-		$user = (object)array
-		(
-			"id" => $result['idUser'],
-			"username" => $result['username'],
-			"name" => $result['name'],
-			"password" => $result['password'],
-			"email" => $result['mail'],
-			"avatar" => $result['avatar']
-		);
-		$users[] = $user;
-	}
-	
-	return $users;
+
+    $users = [];
+
+    $query = $db->prepare("SELECT * FROM mention NATURAL JOIN user WHERE mention.idTweet = :pid");
+    $query->bindValue(":pid", $pid);
+    $query->execute();
+
+    while ($result = $query->fetch()) {
+        $user = (object)array(
+            "id" => $result['idUser'],
+            "username" => $result['username'],
+            "name" => $result['name'],
+            "password" => $result['password'],
+            "email" => $result['mail'],
+            "avatar" => $result['avatar']
+        );
+        $users[] = $user;
+    }
+
+    return $users;
 }
 
 /**
  * Delete a post in db
  * @param id the id of the post to delete
  */
-function destroy($id) {
-	$db = Db::dbc();
+function destroy($id)
+{
+    $db = \Db::dbc();
 
     $query = $db->prepare("DELETE FROM tweet WHERE idTweet = :id");
     $query->bindValue(":id", $id);
     $query->execute();
+
     return true;
 }
 
@@ -167,24 +185,26 @@ function destroy($id) {
  * @param string the string to search in the text
  * @return an array of find objects
  */
-function search($string) {
-    $db = Db::dbc();
+function search($string)
+{
+    $db = \Db::dbc();
 
     $posts = [];
+
     $query = $db->prepare("SELECT * FROM tweet WHERE content LIKE :string");
     $query->bindValue(":string", "%".$string."%");
     $query->execute();
-    while ($result = $query->fetch()) 
-    {
-   		$post = (object) array
-     	(
-        	"id" => $result["idTweet"],
+
+    while ($result = $query->fetch()) {
+        $post = (object) array(
+            "id" => $result["idTweet"],
             "text" => $result["content"],
             "date" => $result["dateTweet"],
             "author" => \Model\User\get($result["idUser"])
-     	);
-     	$posts[] = $post;
+         );
+        $posts[] = $post;
     }
+
     return $posts;
 }
 
@@ -193,28 +213,32 @@ function search($string) {
  * @param date_sorted the type of sorting on date (false if no sorting asked), "DESC" or "ASC" otherwise
  * @return an array of the objects of each post
  */
-function list_all($date_sorted=false) {
+function list_all($date_sorted=false)
+{
     $db = \Db::dbc();
-    if($date_sorted == "ASC")
+
+    $posts = [];
+
+    if ($date_sorted == "ASC") {
         $query = $db->prepare("SELECT * FROM tweet ORDER BY dateTweet ASC");
-    else if($date_sorted == "DESC")
+    } elseif ($date_sorted == "DESC") {
         $query = $db->prepare("SELECT * FROM tweet ORDER BY dateTweet DESC");
-    else
+    } else {
         $query = $db->prepare("SELECT * FROM tweet ORDER BY dateTweet");
+    }
 
     $query->execute();
 
-    while ($result = $query->fetch())
-    {
-      $post = (object) array
-      (
+    while ($result = $query->fetch()) {
+        $post = (object) array(
             "id" => $result["idTweet"],
             "text" => $result["content"],
             "date" => $result["dateTweet"],
             "author" => \Model\User\get($result["idUser"])
           );
-          $posts[] = $post;
+        $posts[] = $post;
     }
+
     return $posts;
 }
 
@@ -224,31 +248,31 @@ function list_all($date_sorted=false) {
  * @param date_sorted the type of sorting on date (false if no sorting asked), "DESC" or "ASC" otherwise
  * @return the list of posts objects
  */
-function list_user_posts($id, $date_sorted="DESC") {
-	$db = \Db::dbc();
+function list_user_posts($id, $date_sorted="DESC")
+{
+    $db = \Db::dbc();
 
-	
-  	if($date_sorted == 'ASC')
-  		$query = $db->prepare("SELECT * FROM tweet WHERE idUser = :id ORDER BY dateTweet ASC");
-	else if($date_sorted == 'DESC')
-    	$query = $db->prepare("SELECT * FROM tweet WHERE idUser = :id ORDER BY dateTweet DESC");
-  	else
-    	$query = $db->prepare("SELECT * FROM tweet WHERE idUser = :id ORDER BY dateTweet");
+    if ($date_sorted == "ASC") {
+        $query = $db->prepare("SELECT * FROM tweet WHERE idUser = :id ORDER BY dateTweet ASC");
+    } elseif ($date_sorted == "DESC") {
+        $query = $db->prepare("SELECT * FROM tweet WHERE idUser = :id ORDER BY dateTweet DESC");
+    } else {
+        $query = $db->prepare("SELECT * FROM tweet WHERE idUser = :id ORDER BY dateTweet");
+    }
 
-  	$query->execute();
+    $query->execute();
 
- 	while ($result = $query->fetch())
-  	{
-    	$post = (object) array
-    	(
+    while ($result = $query->fetch()) {
+        $post = (object) array(
             "id" => $result["idTweet"],
             "text" => $result["content"],
             "date" => $result["dateTweet"],
             "author" => \Model\User\get($result["idUser"])
-    	);
-    	$posts[] = $post;
-  	}
-  	return $posts;
+        );
+        $posts[] = $post;
+    }
+
+    return $posts;
 }
 
 /**
@@ -256,27 +280,28 @@ function list_user_posts($id, $date_sorted="DESC") {
  * @param pid the post's id
  * @return the users objects who liked the post
  */
-function get_likes($pid) {
+function get_likes($pid)
+{
     $db = \Db::dbc();
-    
+
     $users = [];
+
     $query = $db->prepare("SELECT * FROM likeTweet NATURAL JOIN user WHERE likeTweet.idTweet = :pid");
-    $query->bindValue(':pid', $pid);
+    $query->bindValue(":pid", $pid);
     $query->execute();
-    
-    while($result = $query->fetch())
-    {
-    	$user = (object)array
-    	(
-        	"id" => $result['idUser'],
-            "username" => $result['username'],
-            "name" => $result['name'],
-            "password" => $result['password'],
-            "email" => $result['mail'],
-            "avatar" => $result['avatar']
+
+    while ($result = $query->fetch()) {
+        $user = (object)array(
+            "id" => $result["idUser"],
+            "username" => $result["username"],
+            "name" => $result["name"],
+            "password" => $result["password"],
+            "email" => $result["mail"],
+            "avatar" => $result["avatar"]
         );
-    	$users[] = $user;
+        $users[] = $user;
     }
+
     return $users;
 }
 
@@ -285,31 +310,34 @@ function get_likes($pid) {
  * @param pid the post's id
  * @return the posts objects which are a response to the actual post
  */
-function get_responses($pid) {
-    $db = Db::dbc();
-    
+function get_responses($pid)
+{
+    $db = \Db::dbc();
+
     $posts = [];
+
     $query = $db->prepare("SELECT * FROM tweet WHERE idTweet_To = :pid");
     $query->bindValue(":pid", $pid);
     $query->execute();
-    while ($result = $query->fetch()) 
-    {
-		$post = (object) array
-		(
-			"id" => $result["idTweet"],
-			"text" => $result["content"],
-			"date" => $result["dateTweet"],
-			"author" => \Model\User\get($result["idUser"])
-		);
-		$posts[] = $post;
+
+    while ($result = $query->fetch()) {
+        $post = (object) array(
+            "id" => $result["idTweet"],
+            "text" => $result["content"],
+            "date" => $result["dateTweet"],
+            "author" => \Model\User\get($result["idUser"])
+        );
+        $posts[] = $post;
     }
+
     return $posts;
 }
 
 /**
  * Get stats from a post (number of responses and number of likes
  */
-function get_stats($pid) {
+function get_stats($pid)
+{
     return (object) array(
         "nb_likes" => 10,
         "nb_responses" => 40
@@ -321,12 +349,13 @@ function get_stats($pid) {
  * @param uid the user's id to like the post
  * @param pid the post's id to be liked
  */
-function like($uid, $pid) {
+function like($uid, $pid)
+{
     $db = \Db::dbc();
 
     $query = $db->prepare("INSERT INTO likeTweet (idUser, idTweet) VALUES(:uid, :pid)");
-    $query->bindValue(':uid', $uid);
-    $query->bindValue(':pid', $pid);
+    $query->bindValue(":uid", $uid);
+    $query->bindValue(":pid", $pid);
 
     $query->execute();
 
@@ -338,15 +367,15 @@ function like($uid, $pid) {
  * @param uid the user's id to unlike the post
  * @param pid the post's id to be unliked
  */
-function unlike($uid, $pid) {
+function unlike($uid, $pid)
+{
     $db = \Db::dbc();
 
     $query = $db->prepare("DELETE FROM likeTweet WHERE idUser = :uid AND idTweet = :pid");
-    $query->bindValue(':uid', $uid);
-    $query->bindValue(':pid', $pid);
-    
+    $query->bindValue(":uid", $uid);
+    $query->bindValue(":pid", $pid);
+
     $query->execute();
-    
+
     return true;
 }
-
